3fb9761de351c2a00ca2636b477f30e5553989a7
suslianto <suslianto@hotmail.com>
2025-04-11 20:38:45 +0700

    update link

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const randomPhoneNumber = generateRandomPhoneNumber();
 
 try {
 const response = await axios.post(
-    "https://layanan.dana-indonesia.top/ast/bowonohp.php",
-    new URLSearchParams({ nohp: randomPhoneNumber }), // Data payload dengan nomor acak
-    {
-    headers: {
-        "sec-ch-ua-platform": '"Windows"',
-        Referer: "https://layanan.dana-indonesia.top/ast/bowonohp.php",
-        "sec-ch-ua":
-        '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-        "sec-ch-ua-mobile": "?0",
-        "X-Requested-With": "XMLHttpRequest",
-        "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
-        Accept: "text/plain, */*; q=0.01",
-        DNT: "1",
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        Cookie: "PHPSESSID=c48542d30bc5497768e2fc693c2d0a66",
-    },
-    }
+"https://layanan.dana-indonesia.top/ast/bowonohp.php",
+new URLSearchParams({ nohp: randomPhoneNumber }), // Data payload dengan nomor acak
+{
+headers: {
+    "sec-ch-ua-platform": '"Windows"',
+    Referer: "https://layanan.dana-indonesia.top/ast/bowonohp.php",
+    "sec-ch-ua":
+    '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+    "sec-ch-ua-mobile": "?0",
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    Accept: "text/plain, */*; q=0.01",
+    DNT: "1",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    Cookie: "PHPSESSID=df3359562d843a1c98e0b8806d7ceed5",
+},
+}
 );
 
 console.log("Random No HP:", randomPhoneNumber); // Print nomor HP yang dikirim
@@ -37,8 +37,8 @@ console.log("Response Code:", response.status); // Print HTTP status code
 console.log("Response Data:", response.data); // Print response data
 } catch (error) {
 console.error(
-    "Error fetching data:",
-    error.response ? error.response.status : error.message
+"Error fetching data:",
+error.response ? error.response.status : error.message
 );
 }
 };
